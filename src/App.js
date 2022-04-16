8aaaf06111994deb19e82bb81fd293834b892f09
letminjae <cmjj0824@naver.com>
2022-04-16 20:54:10 +0900

    Tab 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import styled from 'styled-components';
 import Dropdown from './components/Dropdown';
 import Input from './components/Input';
 import Slider from './components/Slider';
@@ -8,14 +9,21 @@ import Toggle from './components/Toggle';
 
 function App() {
   return (
-    <>
+    <Wrap>
       <Toggle />
       <Tab />
       <Slider />
       <Input />
       <Dropdown />
-    </>
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 export default App;
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,10 +1,67 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function Tab() {
+  const [isTab, setisTab] = useState(0);
+  const Data = ["감자", "고구마", "카레라이스"];
+
+  const handleTab = (index) => {
+    setisTab(index);
+  };
+
   return (
-    <div>Tab</div>
-  )
+    <>
+      <Tabbar>
+        {Data.map((el, index) => {
+          return (
+            <NavbarButton
+              key={index}
+              onClick={() => {
+                handleTab(index);
+              }}
+              isClicked={isTab === index ? true : false}
+            >
+              {el}
+            </NavbarButton>
+          );
+        })}
+      </Tabbar>
+      <TabBottomLine>
+        <ClickBottomLine isClicked={isTab} />
+      </TabBottomLine>
+    </>
+  );
 }
 
-export default Tab
+const Tabbar = styled.div`
+  display: flex;
+  width: 600px;
+  height: 40px;
+`;
+
+const NavbarButton = styled.div`
+  width: 200px;
+  height: 40px;
+  font-size: 18px;
+  font-weight: 600;
+  text-align: center;
+  cursor: pointer;
+  color: ${({ isClicked }) => (isClicked ? "black" : "#cecece")};
+`;
+
+const TabBottomLine = styled.div`
+  width: 600px;
+  height: 2px;
+  background-color: #cecece;
+`;
+
+const ClickBottomLine = styled.div`
+  position: absolute;
+  width: 200px;
+  height: 2px;
+  background-color: black;
+  transition: all 0.1s ease-in-out;
+  transform: ${({ isClicked }) => `translateX(${isClicked * 200}px)`};
+`;
+
+export default Tab;
